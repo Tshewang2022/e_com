50d2fd981cfa351531850802c169bce2c2b00beb
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-06-12 17:50:23 +0600

    completed with the fontend setup using the vue.js

---
+++ e_com/fontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <div>
+        <h1 class="font-bold text-red-500">Hello world!</h1>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
