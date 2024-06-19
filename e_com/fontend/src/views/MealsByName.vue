c48794d9e674e5708f9da946fbd8da7e5e593223
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-06-19 22:51:57 +0600

    updated

--- e_com/fontend/src/views/MealsByName.vue
+++ e_com/fontend/src/views/MealsByName.vue
@@ -3,10 +3,21 @@
     <input
       type="text"
       name=""
+      v-model="keyword"
       class="rounded border-2 border-gray-200 w-full"
       placeholder="Search for Meals"
+      @change="searchMeals"
       id=""
     />
   </div>
 </template>
-<script></script>
+<script>
+import axiosClient from "../axiosClient";
+
+const keyword = ref("");
+
+// will create the function that will search the meals
+function searchMeals() {
+  axiosClient.get(`search.php?s=${keyword.value}`);
+}
+</script>
