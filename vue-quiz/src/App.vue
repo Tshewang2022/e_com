f6b72b0788b9aff4a9d728f6e6f6733f021719f2
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-07-01 11:45:41 +0600

    props and the defineProps should work hand i hand

--- vue-quiz/src/App.vue
+++ vue-quiz/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from "./data/quizes.json";
 import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 
 const quizes = ref(q);
 
@@ -22,13 +23,15 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="math" />
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+      <!--  v-for="quiz in quizes" :key="quiz.id" -->
+      <!-- <div class="card">
+        <img src="quiz.img" alt="math" />
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }}questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -54,32 +57,5 @@ header input {
   background-color: rgba(128, 128, 128, 0.1);
   padding: 10px;
   border-radius: 5px;
-}
-
-/*CARD */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card .card-text {
-  padding: 0 5px;
-}
-.card .card-text h2 {
-  font-weight: bold;
-}
-.options-container {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 40px;
 }
 </style>
