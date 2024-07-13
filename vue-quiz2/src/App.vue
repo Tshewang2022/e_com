237146d8172697475f927ac576eb41e61820e072
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-07-13 12:40:41 +0600

    defineProps

--- vue-quiz2/src/App.vue
+++ vue-quiz2/src/App.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from "vue";
 // this is just the raw data
 import q from "./data/quizes.json";
+import Card from './components/Card.vue';
 
 // we need to store inside something called state, where we need to store so that we can shared inside it
 const quizes = ref(q);
@@ -16,44 +17,10 @@ quizes.value = q.filter((quiz)=>{return quiz.name.toLowerCase().includes(search.
   // console.log('hello from  watch');
 })
 
-const showQuestion = ref(true);
-const toggleVisibility = () => {
-  showQuestion.value = !showQuestion.value;
-};
+
 </script>
 <template>
-  <div class="container relative">
-    <div v-show="showQuestion">
-      <div class="question-container">
-        <h1>2+2+3840</h1>
-      </div>
-      <div class="answer-container">
-        <div class="flex justify-start items-center gap-[32px]">
-          <p>A</p>
-          <div>
-            <p>16</p>
-          </div>
-        </div>
-        <div class="flex justify-start items-center gap-[32px]">
-          <p>B</p>
-          <div>
-            <p>20</p>
-          </div>
-        </div>
-        <div class="flex justify-start items-center gap-[32px]">
-          <p>C</p>
-          <div>
-            <p>24</p>
-          </div>
-        </div>
-        <div class="flex justify-start items-center gap-[32px]">
-          <p>D</p>
-          <div>
-            <p>30</p>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="container relative"> 
     <header class="flex justify-start gap-[32px] mt-[30px] mb-[10px]">
       <h1>Quizes</h1>
       <input v-model.trim="search"
@@ -67,13 +34,14 @@ const toggleVisibility = () => {
     <div
       class="cards flex justify-center items-center gap-[32px] max-w-[1050px] sm:w-flex-col"
     >
-      <div v-for="quiz in quizes" :key="quiz.id">
+    <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id">
         <img :src="quiz.img" alt="math" />
         <div>
           <h2>{{ quiz.name }}</h2>
           <p>3 Questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
---
+++ vue-quiz2/src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {defineProps} from 'vue';
+const {quiz} = defineProps(['quiz']);
+// console.log(quiz); console is playing very important role in the web development, all most 90% error handling can be done using it
+
+</script>
+<template>
+    <!-- how are we going to pass the data from the parent component to the child component -->
+    <div>
+        <img :src="quiz.img" alt="" />
+        <div>
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div>
+</template>
